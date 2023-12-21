bb979c3a1a9a7951e739917506b71c54868b3d0c
bochrap <131978926+bochrap@users.noreply.github.com>
2023-12-21 11:26:50 +0000

    add form with event listener

--- client/app.js
+++ client/app.js
@@ -1,1 +1,8 @@
-console.log("Hi! How are you?");
+const form = document.getElementById("guestbook-form");
+
+form.addEventListener("submit", async function (event) {
+  event.preventDefault();
+  const formData = new FormData(form);
+  const formValues = Object.fromEntries(formData);
+  console.log(formValues);
+});
