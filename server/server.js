9bcf312f36c00aa13c519d2f6e5d20870344cd9e
bochrap <131978926+bochrap@users.noreply.github.com>
2023-12-21 10:38:44 +0000

    create server endpoints

--- server/server.js
+++ server/server.js
@@ -8,3 +8,15 @@ app.use(cors());
 app.listen(8080, function () {
   console.log("Server is listening at http://localhost:8080");
 });
+
+app.get("/", function (reqest, response) {
+  response.json("You are indeed in my root folder");
+});
+
+app.get("/entries", function (request, response) {
+  response.json("Database guestbook entries going here");
+});
+
+app.post("/entries", function (request, response) {
+  response.json("Where we add new entries to database");
+});
