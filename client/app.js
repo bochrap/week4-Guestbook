de1c7c0f04efca6db9f899b13a0cc8908c908eac
bochrap <131978926+bochrap@users.noreply.github.com>
2023-12-22 10:52:01 +0000

    display all fetched entries om the website

--- client/app.js
+++ client/app.js
@@ -1,4 +1,5 @@
 const form = document.getElementById("guestbook-form");
+const postDiv = document.getElementById("posts-container");
 
 form.addEventListener("submit", async function (event) {
   event.preventDefault();
@@ -16,3 +17,38 @@ form.addEventListener("submit", async function (event) {
   const json = await response.json();
   console.log(json);
 });
+
+async function getEntries() {
+  const response = await fetch("http://localhost:8080/entries");
+  const posts = await response.json();
+  console.log(posts);
+
+  posts.forEach(function (post) {
+    const div = document.createElement("div");
+    div.classList.add(`#${post.id}`); //? do I need this class ?
+
+    for (let i = 0; i <= posts.length; i++) {
+      const span = document.createElement("span");
+      div.appendChild(span);
+      if (i == 0) {
+        span.textContent = `#${post.id}`;
+      } else if (i == 1) {
+        span.textContent = `${post.reaction}`;
+      } else if (i == 2) {
+        span.textContent = `${post.username}:`;
+      } else if (i == 3) {
+        span.textContent = `${post.message}`;
+      } else if (i == 4) {
+        if (post.likes == null) {
+          span.textContent = `0`;
+        } else {
+          span.textContent = `${post.likes}`;
+        }
+      }
+    }
+
+    postDiv.appendChild(div);
+  });
+}
+
+getEntries();
